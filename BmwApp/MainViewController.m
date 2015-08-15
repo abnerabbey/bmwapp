b338045b0914298f316997fa96c90d5d09565c83
Abner Castro <abner.abbey@hotmail.com>
2015-08-15 14:57:41 -0700

    mainview

---
+++ BmwApp/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  MainViewController.m
+//  BmwApp
+//
+//  Created by Abner Castro Aguilar on 15/08/15.
+//  Copyright (c) 2015 Abner Castro Aguilar. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+}
+
+#pragma mark TableView Delegate
+
+
+@end
